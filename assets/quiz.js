cb031a28fd97cab431adb02b29a9fffd3416d0a4
BeeNorrg <brandon.n.norrgard@gmail.com>
2021-02-22 21:16:10 -0600

    added questions object

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,6 +1,6 @@
 //Timer Function
 function countDown() {
-  var minute = 4;
+  var minute = 2;
   var sec = 59;
   setInterval(function() {
     document.getElementById("timer").innerHTML = minute + " : " + sec;
@@ -9,15 +9,26 @@ function countDown() {
       minute --;
       sec = 59;
       if (minute == 0) {
-        minute = 5;
+        minute = 0;
       }
     }
   }, 1000);
 };
 
+//quiz questions
+let questions = {
+  q1: "How can you tell different species of honey bees apart? ",
+  a1: ["how many stripes they have", "the sound they make", "what kinds of flowers they make honey out of"],
+  q2: "What do honey bees eat?",
+  a2: ["honey, duh!", "pollen", "smaller insects"],
+  q3: "why are honey bees going extinct in north america?",
+  a3: ["colony collapse disorder from ingesting commonly used pesticides", "local extinctions due to climate change", "invasive wasp species", "all of the above",],
+}
+
 //replaces "Start Quiz!" button with timer
 $("#start").click(function replacer () {
   $("button.getsReplaced").replaceWith( "<p> you have<span id='timer'> 5:00 </span>remaining! </p>");
 });
+
 //starts the quiz timer
 $("#start").click(countDown());
