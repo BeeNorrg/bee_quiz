637ba6bb36c5b511adbfbf184ce83acc3d1e5b13
BeeNorrg <brandon.n.norrgard@gmail.com>
2021-02-22 22:51:20 -0600

    turned questions variable into an array of objects

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,6 +1,6 @@
 //Timer Function
 function countDown() {
-  var minute = 2;
+  var minute = 1;
   var sec = 59;
   setInterval(function() {
     document.getElementById("timer").innerHTML = minute + " : " + sec;
@@ -15,22 +15,25 @@ function countDown() {
   }, 1000);
 };
 
-//quiz questions
-let questions = {
-  q1: "How can you tell different species of honey bees apart? ",
-  a1: ["how many stripes they have", "the sound they make", "what kinds of flowers they make honey out of"],
-  q2: "What do honey bees eat?",
-  a2: ["honey, duh!", "pollen", "smaller insects"],
-  q3: "why are honey bees going extinct in north america?",
-  a3: ["colony collapse disorder from ingesting commonly used pesticides", "local extinctions due to climate change", "invasive wasp species", "all of the above",],
-}
+//quiz questions array of question objects
+let questions = [ 
+{"q1":"How can you tell different species of honey bees apart?", 
+"a1":["how many stripes they have", "the sound they make", "what flowers they make honey out of"]},
+{"q2":"What do honey bees eat?",
+"a2":["honey, duh!", "pollen", "smaller insects"]},
+{"q3":"why are honey bees going extinct in north america?",
+"a3":["agricultural pollution", "climate change", "invasive wasp species"]},
+];
 
 //replaces "Start Quiz!" button with timer
 $("#start").click(function replacer () {
   $("button.getsReplaced").replaceWith( "<p> you have<span id='timer'> 5:00 </span>remaining! </p>");
 });
 
-//starts the quiz timer
+//button also starts the quiz timer
 $("#start").click(countDown());
 
-
+//the first question is appended to the bodyBox when button is pressed
+$("start").click(function appendQuiz() {
+  $("#quiz").append("<div class='question'>" + questions.q1,"</div>")
+});
