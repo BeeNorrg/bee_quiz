1554933a6e06d67f8f6f2d8874967e6dd9ddd3a4
BeeNorrg <brandon.n.norrgard@gmail.com>
2021-02-28 18:09:32 -0600

    pressing the start button now correctly populates the first quiz quesiton

--- assets/quiz.js
+++ assets/quiz.js
@@ -18,7 +18,9 @@ function countDown() {
   }, 1000);
 };
 
-//quiz questions variables
+
+
+//quiz questions
 const question1 = {
 q:"How do you tell different species of honey bees apart?", a:["how many stripes they have", "the sound they make", "the size of their stinger", 
 "what flowers they make honey out of",]};
@@ -29,31 +31,52 @@ q:"why are honey bees going extinct in north america?", a:["industrial pesticide
 
 //variable to listen for start button being pressed
 const start = document.getElementById("start");
-//variable to listen for a question being answered
-//const chosenAnswer = document.getElementsByClassName("answerButton");
-//HTML table that the quiz will be in
-const quizBlock = "<button class='button is-primary answerButton'>A</button><p id='answerA'></p><button class='button is-primary answerButton'>B</button><p id='answerB'></p><button class='button is-primary answerButton'>C</button><p id='answerC'></p><button class='button is-primary answerButton'>D</button><p id='answerD'></p>";
+//variable to listen for the "submit answer" button being pressed
+const submit = document.getElementById("nextButton");
+//varaible to store whether or not a question has been answered
+const answerButton = doecument.getElementsbyClassName("quizButton")
+//object to initialize the quiz's html
+const qHTML = {
+buttons:["<button class='button is-primary quizButton'>A</button>","<button class='button is-primary quizButton'>B</button>","<button class='button is-primary quizButton'>C</button>","<button class='button is-primary quizButton'>D</button>"],
+answerDivs:["<p id='answerA'></p>","<p id='answerB'></p>","<p id='answerC'></p>","<p id='answerD'></p>"]
+};
 
 //eventlistener to start the quiz timer
-start.addEventListener('click', function () {
+start.addEventListener("click", function startTimer() {
   $("button.getsReplaced").replaceWith("<p> you have<span id='timer'> 2 : 00 </span>remaining! </p>");
   countDown();
 });
 //eventlistener to post first question
-start.addEventListener('click', function() {
-  $("#quiz").append("<div>" + quizBlock + "</div>");
-  $("#questionText").append("<span>" + question1.q + "</span>");
-  for (i=0; i <= question1.a.length; i++) {
-    if (i=1) {
-      $("#answerA").append(question1.a[0])
-    } if (i=2) {
-      $("#answerB").append(question1.a[1])
-    } if (i=3) {
-      $("#answerC").append(question1.a[2])
-    } if (i=4) {
-      $("answerD").append(question1.a[3])
+start.addEventListener('click', function startQuiz() {
+  $("#quiz").append("<div class='qTitle'>" + question1.q + "</div>");
+  for (i=1; i <= 4; i++) {
+    $("#quiz").append("<div class='quizDiv' id='a" + i  +"'></div>");
+    if (i===1) {
+      $("#a1").append(qHTML.buttons[0]  + qHTML.answerDivs[0])
+    } if (i===2) {
+      $("#a2").append(qHTML.buttons[1] + qHTML.answerDivs[1])
+    } if (i===3) {
+      $("#a3").append(qHTML.buttons[2] + qHTML.answerDivs[2])
+    } if (i===4) {
+      $("#a4").append(qHTML.buttons[3] + qHTML.answerDivs[3])
     };
   };
+  // Populates the answer divs with question 1 answers
+  for (i=0; i <= question1.a.length; i++) {
+    if (i===0) {
+      $("#answerA").append(question1.a[0])
+    } if (i===1) {
+      $("#answerB").append(question1.a[1])
+    } if (i===2) {
+      $("#answerC").append(question1.a[2])
+    } else if (i===3) {
+      $("#answerD").append(question1.a[3])
+    };
+  };
+  // shows we're on the first question
+  const ready1 = true;
 });
-//eventlistener to check if a question has been answered 
-//chosenAnswer.addEventListener('click', function nextQuestion () {});
+
+
+
+
